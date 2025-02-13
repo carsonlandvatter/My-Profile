859b99bb0b8e9bd5a414605deceef21d5526a0df
carsonlandvatter <160798607+carsonlandvatter@users.noreply.github.com>
2025-02-13 09:31:48 -0800

    Add files via upload
    file upload

---
+++ Portfolio Website/script.js
@@ -0,0 +1,5 @@
+let learnMoreButton = document.getElementById("learnMore");
+
+learnMoreButton.addEventListener("click", function() {
+    document.getElementById("intro").innerHTML += "<p>I am currently enrolled in BYU Idaho Online and and am studying to get my Bachelor's Degree in Software Development. I also have other skills from my work experience that involve teamwork, problem-solving, and using technology to help others.</p>";
+});
